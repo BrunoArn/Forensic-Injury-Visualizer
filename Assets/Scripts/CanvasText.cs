b2c52be8fe023cd61599a58cf9a76bb7e44f15ae
Bruno Arnaud <bruno.arnaud.br@gmail.com>
2025-05-22 16:00:00 -0300

    new UI

--- Assets/Scripts/CanvasText.cs
+++ Assets/Scripts/CanvasText.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class CanvasText : MonoBehaviour
 {
+    [Header("Title configs")]
     [SerializeField] GameObject title;
+    [Header("text configs")]
+    [SerializeField] Image textBackgorund;
     [SerializeField] GameObject textBody;
+    [SerializeField] float heightOffset;
+    [Header("Image configs")]
     [SerializeField] GameObject textImage;
 
     private TextMeshProUGUI titleText;
     private TextMeshProUGUI textBodyText;
     private Image imageSprite;
-
-
 
     void Start()
     {
@@ -22,10 +25,28 @@ public class CanvasText : MonoBehaviour
         imageSprite = textImage.GetComponent<Image>();
     }
 
-    public void UpdateText(string newTitle, string newText, Sprite newImage) {
+    private void MatchBackgroundWithTextHeight()
+    {
+        if (textBodyText == null || textBackgorund == null) return;
+
+        // Get preferred height of the TMP text
+        float textHeight = textBodyText.preferredHeight;
+
+        // Get the Image RectTransform
+        RectTransform imageRect = textBackgorund.rectTransform;
+
+        // Match height with offset
+        Vector2 size = imageRect.sizeDelta;
+        size.y = textHeight + heightOffset;
+        imageRect.sizeDelta = size;
+    }
+
+    public void UpdateText(string newTitle, string newText, Sprite newImage)
+    {
         titleText.text = newTitle;
         textBodyText.text = newText;
         imageSprite.sprite = newImage;
+        MatchBackgroundWithTextHeight();
     }
 
     internal void UpdateText()
